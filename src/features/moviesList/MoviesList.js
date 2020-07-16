e293f12acb8801ac03006392e858dce5c8aea7b1
Erick Martinez Jr <erick@erickjr.com>
2020-07-16 09:49:02 -0400

    Add memo optimization

--- src/features/moviesList/MoviesList.js
+++ src/features/moviesList/MoviesList.js
@@ -5,8 +5,8 @@ import Grid from '@material-ui/core/Grid';
 
 const MoviesList = ({ movies }) => {
 	const renderedMovies = movies.map(movie => (
-		<Grid item xs={6} md={4} lg={3}>
-			<MovieListItem key={movie.id} {...movie} />
+		<Grid item xs={6} md={4} lg={3} key={movie.id}>
+			<MovieListItem {...movie} />
 		</Grid>
 	));
 
@@ -21,4 +21,4 @@ MoviesList.propTypes = {
 	movies: PropTypes.array.isRequired
 };
 
-export default MoviesList;
+export default React.memo(MoviesList);
